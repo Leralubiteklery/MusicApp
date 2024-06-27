b72566a08fc226b374dd0daf30f98654880afbfc
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2024-06-27 18:42:33 +0300

    Set up table view on Search VC. Added search bar.

--- MusicApp/SearchViewController.swift
+++ MusicApp/SearchViewController.swift
@@ -7,13 +7,56 @@
 
 import UIKit
 
+struct TrackModel{
+    var trackName: String
+    var artistName: String
+}
+
 
 class SearchViewController: UITableViewController {
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    let trackList = [
+        TrackModel(trackName: "About A Girl", artistName: "Nirvana"),
+        TrackModel(trackName: "Love Buzz", artistName: "Nirvana")
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
+        
+        setupSearchBar()
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: " cellId")
+            }
+    
+    private func setupSearchBar() {
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.searchBar.delegate = self
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        trackList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        
+        let track = trackList[indexPath.row]
+        cell.textLabel?.text = "\(track.trackName)\n\(track.artistName)"
+        cell.textLabel?.numberOfLines = 2
+        cell.imageView?.image =  #imageLiteral(resourceName: "nirvana_bleach")
+        return cell
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
     }
     
 }
