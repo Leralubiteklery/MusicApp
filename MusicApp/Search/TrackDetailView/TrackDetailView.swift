ac88044dd51ee0144d27ad492024e1cf66940e56
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2024-07-31 15:59:04 +0300

    Added UI elements for the TrackDetailView.

--- MusicApp/Search/TrackDetailView/TrackDetailView.swift
+++ MusicApp/Search/TrackDetailView/TrackDetailView.swift
@@ -9,7 +9,34 @@ import UIKit
 
 class TrackDetailView: UIView {
     
+    @IBOutlet weak var currentTimeSlider: UISlider!
+    @IBOutlet weak var trackImageView: UIImageView!
+    @IBOutlet weak var currentTimeLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var trackTitleLabel: UILabel!
+    @IBOutlet weak var artistLabel: UILabel!
+    @IBOutlet weak var playPauseButton: UIButton!
+    @IBOutlet weak var volumeSlider: UISlider!
+    
     override class func awakeFromNib() {
         super.awakeFromNib()
+    }
+    @IBAction func dragDownButtonTapped(_ sender: Any) {
+        self.removeFromSuperview()
+    }
+    
+    @IBAction func handleCurrentTimerSlider(_ sender: Any) {
+    }
+    
+    @IBAction func handleVolumeSlider(_ sender: Any) {
+    }
+    
+    @IBAction func previousTrack(_ sender: Any) {
+    }
+    
+    @IBAction func nextTrack(_ sender: Any) {
+    }
+    
+    @IBAction func playPauseAction(_ sender: Any) {
     }
 }
